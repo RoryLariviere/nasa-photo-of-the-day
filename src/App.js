58c16566e8d97308274205d87751210336d2ea81
Rory Lariviere <rorydlar@gmail.com>
2021-02-17 16:36:32 -0600

    setting up initial push, and set up axios fetch and basic header title/synopsis

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import { BASE_URL, API_KEY } from './constants/constants';
+import axios from 'axios';
 
 function App() {
+
+
+
+  // useEffect(()=>{
+  //   const fetchApod = () => {
+  //     axios.get(`${BASE_URL}/planetary/apod?api_key=${API_KEY}`)
+  //       .then(res => {
+  //         console.log(res.data)
+  //       })
+  //       .catch(err => {
+  //         console.log(err)
+  //       })
+  //   };
+  //   fetchApod();
+  // }, [])
+
+
+
+
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <h1>NASA Astronomy Photo of the Day</h1>
+      <p>Each day a different image is displayed of the universe, along with a brief explanation written by a professional astronomer.</p>
     </div>
   );
 }
